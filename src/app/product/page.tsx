4684a924c26e5b8bf84e5675913e466748da0e6b
Lucas <lucas75maia@gmail.com>
2023-08-30 13:49:45 -0300

    update page products

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -31,7 +31,7 @@ const actions = [
 
 export default function Product() {
     return (
-        <>
+        <section className="bg-white min-h-screen my-0 relative w-full relative">
             <div className='flex items-center border-b-2 border-b-gray-50 py-2'>
                 <button
                     type="button"
@@ -41,51 +41,82 @@ export default function Product() {
                     Voltar
                 </button >
             </div>
-            <section className="bg-white min-h-screen my-0 relative w-full relative">
-                <div className="sticky top-0 z-[2] w-full flex flex-col">
-                    {/* <img src="https://cdn.icon-icons.com/icons2/2131/PNG/512/back_arrow_icon_131563.png" alt="ícone de retorno" className="absolute top-4 shadow-lg border-2 p-2 bg-gray-200 rounded-full w-10 left-2" /> */}
-                    <img src="https://www.goomer.app/webmenu/muza-restaurante/product/5055134/picture/large/230826220826" alt="Product" className="w-full h-60 object-cover" />
-                </div>
-                <div className="bg-white flex flex-col gap-2 shadowAlter z-[5] sticky min-h-screen">
-                    <h1 className="text-left text-xl font-semibold mt-4 px-4">2 clássicos pelo preço de 1</h1>
-                    <p className="text-left text-base text-gray-700 px-4">Todas as cervejas do Roister com 30% de desconto. Verifique a disponibilidade atual no nosso cardápio físico.</p>
-                    <span className="text-base text-black font-semibold px-4">R$ 25,90 <span className="ml-2 text-sm text-gray-500 line-through">R$ 35,00</span></span>
-                    <div>
-                        <h2 className="pl-4 my-5 font-semibold text-base">Bebidas</h2>
-                        <div className="divide-y divide-gray-200 overflow-hidden sm:grid grid-cols-2 sm:gap-px sm:divide-y-0 shadowAlter">
-                            {actions.map((action, actionIdx) => (
-                                <div className='flex justify-between items-center p-4' key={action.link + actionIdx}>
-                                    <div
-                                        key={action.title}
-                                        className={classNames(
-                                            'flex justify-start items-center gap-4 group relative bg-white focus-within:ring-2 focus-within:ring-inset'
-                                        )}
-                                    >
-                                        <img src={action.link} alt="" className="w-20 h-20" />
-                                        <div>
-                                            <h3 className="text-sm font-medium leading-6 text-gray-900">
-                                                <a href={action.href} className="focus:outline-none">
-                                                    <span className="absolute inset-0" aria-hidden="true" />
-                                                    {action.title}
-                                                </a>
-                                            </h3>
-                                        </div>
+            <div className="sticky top-0 z-[2] w-full flex flex-col mb-[-10px]">
+                <img src="https://www.goomer.app/webmenu/muza-restaurante/product/5055134/picture/large/230826220826" alt="Product" className="w-full h-60 object-cover" />
+            </div>
+            <div className="bg-white flex flex-col gap-2 shadowAlter z-[5] sticky rounded-xl">
+                <h1 className="text-left text-xl font-semibold mt-4 px-4">2 clássicos pelo preço de 1</h1>
+                <p className="text-left text-base text-gray-700 px-4">Todas as cervejas do Roister com 30% de desconto. Verifique a disponibilidade atual no nosso cardápio físico.</p>
+                <span className="text-base text-black font-semibold px-4">R$ 25,90 <span className="ml-2 text-sm text-gray-500 line-through">R$ 35,00</span></span>
+                <div>
+                    <h2 className="pl-4 my-5 font-semibold text-base">Opcionais</h2>
+                    <div className="divide-y divide-gray-200 overflow-hidden sm:grid grid-cols-2 sm:gap-px sm:divide-y-0 shadowAlter">
+                        {actions.map((action, actionIdx) => (
+                            <div className='flex justify-between items-center p-4' key={action.link + actionIdx}>
+                                <div
+                                    key={action.title}
+                                    className={classNames(
+                                        'flex justify-start items-center gap-4 group relative bg-white focus-within:ring-2 focus-within:ring-inset'
+                                    )}
+                                >
+                                    <img src={action.link} alt="" className="w-20 h-20" />
+                                    <div>
+                                        <h3 className="text-sm font-medium leading-6 text-gray-900">
+                                            <a href={action.href} className="focus:outline-none">
+                                                <span className="absolute inset-0" aria-hidden="true" />
+                                                {action.title}
+                                            </a>
+                                        </h3>
                                     </div>
-                                    <input
-                                        // aria-describedby={`${plan.id}-description`}
-                                        name="plan"
-                                        type="radio"
-                                        checked
-                                        value={'teste'}
-                                        // defaultChecked={plan.id === 'small'}
-                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                    />
                                 </div>
-                            ))}
-                        </div>
+                                <input
+                                    // aria-describedby={`${plan.id}-description`}
+                                    name="plan"
+                                    type="radio"
+                                    checked
+                                    value={'teste'}
+                                    // defaultChecked={plan.id === 'small'}
+                                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                />
+                            </div>
+                        ))}
                     </div>
                 </div>
-            </section>
-        </>
+                <div>
+                    <h2 className="pl-4 my-5 font-semibold text-base">Bebidas</h2>
+                    <div className="divide-y divide-gray-200 overflow-hidden sm:grid grid-cols-2 sm:gap-px sm:divide-y-0 shadowAlter">
+                        {actions.map((action, actionIdx) => (
+                            <div className='flex justify-between items-center p-4' key={action.link + actionIdx}>
+                                <div
+                                    key={action.title}
+                                    className={classNames(
+                                        'flex justify-start items-center gap-4 group relative bg-white focus-within:ring-2 focus-within:ring-inset'
+                                    )}
+                                >
+                                    <img src={action.link} alt="" className="w-20 h-20" />
+                                    <div>
+                                        <h3 className="text-sm font-medium leading-6 text-gray-900">
+                                            <a href={action.href} className="focus:outline-none">
+                                                <span className="absolute inset-0" aria-hidden="true" />
+                                                {action.title}
+                                            </a>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <input
+                                    // aria-describedby={`${plan.id}-description`}
+                                    name="plan"
+                                    type="radio"
+                                    checked
+                                    value={'teste'}
+                                    // defaultChecked={plan.id === 'small'}
+                                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
