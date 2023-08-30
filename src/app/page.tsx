bd87c5443f0000810be38d1a568d28e870896f46
Lucas <lucas75maia@gmail.com>
2023-08-30 14:18:09 -0300

    add video - page product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Video from './components/Video/Video'
 
 export default function Home() {
   const link = "https://classic.exame.com/wp-content/uploads/2016/12/madero-restaurante.jpg?quality=70&strip=info&w=680"
-  const actions = [
+  const products = [
     {
       title: 'Bulgogi Vegano + Chá Gelado',
       href: '#',
@@ -33,14 +33,6 @@ export default function Home() {
     }
   ]
 
-  const steps = [
-    { id: '01', name: 'Entradas', description: 'Vitae sed mi luctus laoreet.', href: '#', status: 'current' },
-    { id: '02', name: 'Saladas', description: 'Cursus semper viverra.', href: '#', status: 'complete' },
-    { id: '03', name: 'Principal', description: 'Cursus semper viverra.', href: '#', status: 'complete' },
-    { id: '04', name: 'Drinks', description: 'Cursus semper viverra.', href: '#', status: 'complete' },
-    { id: '05', name: 'Doces', description: 'Cursus semper viverra.', href: '#', status: 'complete' },
-  ]
-
   function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
   }
@@ -53,13 +45,12 @@ export default function Home() {
     <section className="bg-gray-100 min-h-screen max-w-5xl mx-auto my-0 relative w-full">
       <div className="flex gap-4 pt-3 pb-1.5 items-center bg-white w-full mx-auto px-4">
         <Image src={logo} alt="Logo do site" className="w-20 h-20 " />
-        {/* rounded-full border-2 shadow */}
         <div className='flex flex-col'>
           <h1 className="font-bold text-sm">Roister - Rua 24 de outubro</h1>
           <h2 className="font-medium text-xs ext-gray-300">Cantina & Italiano</h2>
         </div>
       </div>
-      <header className="flex flex-col bg-white top-0 sticky z-10 pt-1.5 shadow-lg ease-linear duration-100" id='header'>
+      <nav className="flex flex-col bg-white top-0 sticky z-10 pt-1.5 shadow-lg ease-linear duration-700 opacity-100" id='header'>
         <section className="flex gap-3.5 min-w-full overflow-auto scroll-remove px-2 pb-2 scrollStyle">
           {cardapioOptions.map((item, index) => (
             <button
@@ -73,33 +64,33 @@ export default function Home() {
             </button>
           ))}
         </section>
-      </header >
+      </nav >
       <main className="flex flex-col ">
         <div>
           <h2 className="pl-4 my-5 font-semibold text-base">Cardápio Almoço</h2>
           <div className="divide-y divide-gray-200 overflow-hidden sm:grid grid-cols-2 sm:gap-px sm:divide-y-0 shadowAlter">
-            {actions.map((action, actionIdx) => (
+            {products.map((product, index) => (
               <div
-                key={action.title}
+                key={product.title}
                 className={classNames(
                   'flex justify-between items-center gap-8 group relative bg-white p-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500'
                 )}
               >
                 <div>
                   <h3 className="text-base font-medium leading-6 text-gray-900">
-                    <a href={action.href} className="focus:outline-none">
+                    <a href={product.href} className="focus:outline-none">
                       <span className="absolute inset-0" aria-hidden="true" />
-                      {action.title}
+                      {product.title}
                     </a>
                   </h3>
                   <p className="mt-2 text-sm text-black font-light">
-                    {action.description}
+                    {product.description}
                   </p>
                   <p className="mt-2 text-sm text-black font-medium	">
-                    R$ {action.price}
+                    R$ {product.price}
                   </p>
                 </div>
-                <img src={action.link} alt="" className="w-20 h-20" />
+                <img src={product.link} alt={"Imagem do produto" + product.title} className="w-20 h-20" />
               </div>
             ))}
           </div>
@@ -108,28 +99,28 @@ export default function Home() {
         <div>
           <h2 className="pl-4 my-5 font-semibold text-base">Cardápio Almoço</h2>
           <div className="divide-y divide-gray-200 overflow-hidden sm:grid grid-cols-2 sm:gap-px sm:divide-y-0 shadowAlter">
-            {actions.map((action, actionIdx) => (
+            {products.map((product, index) => (
               <div
-                key={action.title}
+                key={product.title}
                 className={classNames(
                   'flex justify-between items-center gap-8 group relative bg-white p-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500'
                 )}
               >
                 <div>
                   <h3 className="text-base font-medium leading-6 text-gray-900">
-                    <a href={action.href} className="focus:outline-none">
+                    <a href={product.href} className="focus:outline-none">
                       <span className="absolute inset-0" aria-hidden="true" />
-                      {action.title}
+                      {product.title}
                     </a>
                   </h3>
                   <p className="mt-2 text-sm text-black font-light">
-                    {action.description}
+                    {product.description}
                   </p>
                   <p className="mt-2 text-sm text-black font-medium	">
-                    R$ {action.price}
+                    R$ {product.price}
                   </p>
                 </div>
-                <img src={action.link} alt="" className="w-20 h-20" />
+                <img src={product.link} alt={"Imagem do produto" + product.title} className="w-20 h-20" />
               </div>
             ))}
           </div>
@@ -137,28 +128,28 @@ export default function Home() {
         <div>
           <h2 className="pl-4 my-5 font-semibold text-base">Cardápio Almoço</h2>
           <div className="divide-y divide-gray-200 overflow-hidden sm:grid grid-cols-2 sm:gap-px sm:divide-y-0 shadowAlter">
-            {actions.map((action, actionIdx) => (
+            {products.map((product, index) => (
               <div
-                key={action.title}
+                key={product.title}
                 className={classNames(
                   'flex justify-between items-center gap-8 group relative bg-white p-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500'
                 )}
               >
                 <div>
                   <h3 className="text-base font-medium leading-6 text-gray-900">
-                    <a href={action.href} className="focus:outline-none">
+                    <a href={product.href} className="focus:outline-none">
                       <span className="absolute inset-0" aria-hidden="true" />
-                      {action.title}
+                      {product.title}
                     </a>
                   </h3>
                   <p className="mt-2 text-sm text-black font-light">
-                    {action.description}
+                    {product.description}
                   </p>
                   <p className="mt-2 text-sm text-black font-medium	">
-                    R$ {action.price}
+                    R$ {product.price}
                   </p>
                 </div>
-                <img src={action.link} alt="" className="w-20 h-20" />
+                <img src={product.link} alt={"Imagem do produto" + product.title} className="w-20 h-20" />
               </div>
             ))}
           </div>
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,33 +1,6 @@
+import Video from '../components/Video/Video'
+
 import { StarIcon, CheckCircleIcon, ChevronLeftIcon } from '@heroicons/react/20/solid'
-
-function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ')
-}
-
-const actions = [
-    {
-        title: 'Suco de abacaxi',
-        href: '#',
-        link: "https://tagmeimages.azureedge.net/?q=70&url=https://tagmepub.azureedge.net/pubimg/thumbs/MenuItem/523786c0-aebf-11ec-b99f-5504017ee26a.jpg&w=80&h=80&output=jpg&dpr=2",
-        description: "cogumelos, mini pimentões e cebola roxa tostados e pimenta gochujang... ",
-        price: '49,00'
-    },
-    {
-        title: 'Refrigerante',
-        href: '#',
-        link: "https://tagmeimages.azureedge.net/?q=70&url=https://tagmepub.azureedge.net/pubimg/thumbs/MenuItem/5e553b10-18b3-11ed-8c61-b7f633bde1ba.jpg&w=80&h=80&output=jpg&dpr=2",
-        description: "Frango empanado (230g) • molho de queijos • house fries",
-        price: '49,00'
-    },
-    {
-        title: 'Chá Gelado',
-        description: "ragu de carne • batata baby confit • picles de moranga • farofa de pão • couve",
-        link: "https://tagmeimages.azureedge.net/?q=70&url=https://tagmepub.azureedge.net/pubimg/thumbs/MenuItem/81b49120-afa2-11ec-827e-bfa70ccd62ce.jpg&w=80&h=80&output=jpg&dpr=2",
-        href: '#',
-        price: '100,00'
-    }
-]
-
 
 export default function Product() {
     return (
@@ -41,80 +14,15 @@ export default function Product() {
                     Voltar
                 </button >
             </div>
-            <div className="sticky top-0 z-[2] w-full flex flex-col mb-[-10px]">
+            <div className="sticky top-0 z-10 w-full flex flex-col">
                 <img src="https://www.goomer.app/webmenu/muza-restaurante/product/5055134/picture/large/230826220826" alt="Product" className="w-full h-60 object-cover" />
             </div>
-            <div className="bg-white flex flex-col gap-2 shadowAlter z-[5] sticky rounded-xl">
+            <div className="bg-white flex flex-col gap-2 shadowAlter z-20 sticky">
                 <h1 className="text-left text-xl font-semibold mt-4 px-4">2 clássicos pelo preço de 1</h1>
                 <p className="text-left text-base text-gray-700 px-4">Todas as cervejas do Roister com 30% de desconto. Verifique a disponibilidade atual no nosso cardápio físico.</p>
                 <span className="text-base text-black font-semibold px-4">R$ 25,90 <span className="ml-2 text-sm text-gray-500 line-through">R$ 35,00</span></span>
-                <div>
-                    <h2 className="pl-4 my-5 font-semibold text-base">Opcionais</h2>
-                    <div className="divide-y divide-gray-200 overflow-hidden sm:grid grid-cols-2 sm:gap-px sm:divide-y-0 shadowAlter">
-                        {actions.map((action, actionIdx) => (
-                            <div className='flex justify-between items-center p-4' key={action.link + actionIdx}>
-                                <div
-                                    key={action.title}
-                                    className={classNames(
-                                        'flex justify-start items-center gap-4 group relative bg-white focus-within:ring-2 focus-within:ring-inset'
-                                    )}
-                                >
-                                    <img src={action.link} alt="" className="w-20 h-20" />
-                                    <div>
-                                        <h3 className="text-sm font-medium leading-6 text-gray-900">
-                                            <a href={action.href} className="focus:outline-none">
-                                                <span className="absolute inset-0" aria-hidden="true" />
-                                                {action.title}
-                                            </a>
-                                        </h3>
-                                    </div>
-                                </div>
-                                <input
-                                    // aria-describedby={`${plan.id}-description`}
-                                    name="plan"
-                                    type="radio"
-                                    checked
-                                    value={'teste'}
-                                    // defaultChecked={plan.id === 'small'}
-                                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div>
-                    <h2 className="pl-4 my-5 font-semibold text-base">Bebidas</h2>
-                    <div className="divide-y divide-gray-200 overflow-hidden sm:grid grid-cols-2 sm:gap-px sm:divide-y-0 shadowAlter">
-                        {actions.map((action, actionIdx) => (
-                            <div className='flex justify-between items-center p-4' key={action.link + actionIdx}>
-                                <div
-                                    key={action.title}
-                                    className={classNames(
-                                        'flex justify-start items-center gap-4 group relative bg-white focus-within:ring-2 focus-within:ring-inset'
-                                    )}
-                                >
-                                    <img src={action.link} alt="" className="w-20 h-20" />
-                                    <div>
-                                        <h3 className="text-sm font-medium leading-6 text-gray-900">
-                                            <a href={action.href} className="focus:outline-none">
-                                                <span className="absolute inset-0" aria-hidden="true" />
-                                                {action.title}
-                                            </a>
-                                        </h3>
-                                    </div>
-                                </div>
-                                <input
-                                    // aria-describedby={`${plan.id}-description`}
-                                    name="plan"
-                                    type="radio"
-                                    checked
-                                    value={'teste'}
-                                    // defaultChecked={plan.id === 'small'}
-                                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                />
-                            </div>
-                        ))}
-                    </div>
+                <div className='mt-5'>
+                    <Video indexVideo={0} />
                 </div>
             </div>
         </section>
