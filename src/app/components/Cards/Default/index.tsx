de882049b704b2c58a92542fa329d0d41a04048f
Lucas <lucas75maia@gmail.com>
2023-10-02 20:09:37 -0300

    envolvendo funções no useCallback

--- src/app/components/Cards/Default/index.tsx
+++ src/app/components/Cards/Default/index.tsx
@@ -2,39 +2,39 @@
 import Link from "next/link"
 
 function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ')
 }
 
 export default function Cards({ products, title, buttonId, idElement }: { products: product[], title: string, buttonId: string, idElement: string }) {
 
-    return (
-        <div id={idElement} className="scroll-mt-14">
-            <h2 className="pl-4 my-5 font-semibold text-base" data-anime={buttonId}>{title}</h2>
-            <div className="grid divide-y divide-gray-200 overflow-hidden grid-cols-1 gap-px shadowAlter">
-                {products.map((product, index) => (
-                    <Link href={"/product"} key={product.title}>
-                        <div
-                            className={classNames(
-                                'flex justify-between items-center gap-8 group relative bg-white p-4'
-                            )}
-                        >
-                            <div>
-                                <h3 className="text-base font-medium leading-6 text-gray-900">
-                                    <span className="absolute inset-0" aria-hidden="true" />
-                                    {product.title}
-                                </h3>
-                                <p className="mt-2 text-sm text-black font-light">
-                                    {product.description}
-                                </p>
-                                <p className="mt-2 text-sm text-black font-medium	">
-                                    R$ {product.price}
-                                </p>
-                            </div>
-                            <img src={product.link} alt={"Imagem do produto" + product.title} className="w-20 h-20" />
-                        </div>
-                    </Link>
-                ))}
+  return (
+    <div id={idElement} className="scroll-mt-14">
+      <h2 className="pl-4 my-5 font-semibold text-base" data-anime={buttonId}>{title}</h2>
+      <div className="grid divide-y divide-gray-200 overflow-hidden grid-cols-1 gap-px shadowAlter">
+        {products.map((product, index) => (
+          <Link href={"/product"} key={product.title}>
+            <div
+              className={classNames(
+                'flex justify-between items-center gap-8 group relative bg-white p-4'
+              )}
+            >
+              <div>
+                <h3 className="text-base font-medium leading-6 text-gray-900">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  {product.title}
+                </h3>
+                <p className="mt-2 text-sm text-black font-light">
+                  {product.description}
+                </p>
+                <p className="mt-2 text-sm text-black font-medium	">
+                  R$ {product.price}
+                </p>
+              </div>
+              <img src={product.link} alt={"Imagem do produto" + product.title} className="w-20 h-20" />
             </div>
-        </div >
-    )
+          </Link>
+        ))}
+      </div>
+    </div >
+  )
 }
--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -3,79 +3,79 @@ import React, { useEffect, useRef, useState, useCallback } from 'react';
 import Link from 'next/link';
 
 const Nav = React.forwardRef(({ navItems }: { navItems: string[] }, ref: React.Ref<HTMLDivElement>) => {
-    const [buttonSelected, setButtonSelected] = useState(0)
+  const [buttonSelected, setButtonSelected] = useState(0)
 
-    function scrollToSection(elementId: string, index: number) {
-        const element = document.getElementById(elementId);
-        let nav = document.getElementById('nav')
+  const scrollToSection = useCallback((elementId: string, index: number) => {
+    const element = document.getElementById(elementId);
+    let nav = document.getElementById('nav')
 
-        if (element) {
-            nav ? nav.scrollLeft = element.offsetLeft - 2 : null
-            setButtonSelected(index)
-        }
+    if (element) {
+      nav ? nav.scrollLeft = element.offsetLeft - 2 : null
+      setButtonSelected(index)
     }
+  }, [])
 
-    function scrollToSectionNew(elementId: string) {
-        const element = document.getElementById(elementId)?.offsetTop;
+  const scrollToSectionNew = useCallback((elementId: string) => {
+    const element = document.getElementById(elementId)?.offsetTop;
 
-        if (element) {
-            window.scroll({
-                top: element - 50,
-                behavior: "smooth"
-            })
-        }
+    if (element) {
+      window.scroll({
+        top: element - 50,
+        behavior: "smooth"
+      })
     }
+  }, [])
 
-    function animeScroll() {
-        const items = document.querySelectorAll('[data-anime]') as NodeListOf<HTMLHeadElement>
-        const windowTop = window.scrollY
+  const animeScroll = useCallback(() => {
+    const items = document.querySelectorAll('[data-anime]') as NodeListOf<HTMLHeadElement>
+    const windowTop = window.scrollY
 
-        for (let index = 0; index < items.length; index++) {
-            if (index == 0 && index != items.length - 1) {
-                if (windowTop >= items[index].offsetTop - 100 && windowTop < items[index + 1].offsetTop - 100) {
-                    scrollToSection(items[index].attributes[1].value, index)
-                }
-
-            }
-
-            else if (index == items.length - 1) {
-                if (windowTop > (items[index].offsetTop - 300)) {
-                    scrollToSection(items[index].attributes[1].value, index)
-                }
-            }
-            else {
-                if (windowTop >= items[index].offsetTop - 100 && windowTop < items[index + 1].offsetTop - 100) {
-                    scrollToSection(items[index].attributes[1].value, index)
-                }
-            }
+    for (let index = 0; index < items.length; index++) {
+      if (index == 0 && index != items.length - 1) {
+        if (windowTop >= items[index].offsetTop - 100 && windowTop < items[index + 1].offsetTop - 100) {
+          scrollToSection(items[index].attributes[1].value, index)
         }
+
+      }
+
+      else if (index == items.length - 1) {
+        if (windowTop > (items[index].offsetTop - 300)) {
+          scrollToSection(items[index].attributes[1].value, index)
+        }
+      }
+      else {
+        if (windowTop >= items[index].offsetTop - 100 && windowTop < items[index + 1].offsetTop - 100) {
+          scrollToSection(items[index].attributes[1].value, index)
+        }
+      }
     }
+  }, [])
 
-    useEffect(() => {
-        window.addEventListener('scroll', animeScroll)
+  useEffect(() => {
+    window.addEventListener('scroll', animeScroll)
 
-        return () => window.removeEventListener("scroll", animeScroll);
-    }, [])
+    return () => window.removeEventListener("scroll", animeScroll);
+  }, [])
 
-    return (
-        <nav className="flex flex-col bg-white top-0 sticky z-10 pt-1.5 shadow-lg ease-linear duration-700 opacity-100" id='header' ref={ref}>
-            <section className="flex gap-3.5 min-w-full overflow-auto scroll-remove px-2 pb-2 scrollStyle" id='nav'>
-                {navItems.map((item, index) => (
-                    <button
-                        key={item}
-                        type="button"
-                        onClick={() => { scrollToSectionNew(item) }}
-                        id={`button${item}`}
-                        className={`${index == buttonSelected ?
-                            "bg-white px-8 py-2 text-sm font-bold text-black w-auto border-black border-2 whitespace-nowrap buttons" :
-                            "bg-white px-8 py-2 text-sm font-medium text-gray-700 border-2 w-auto whitespace-nowrap buttons"} `}
-                    >
-                        {item}
-                    </button>
-                ))}
-            </section>
-        </nav >
-    )
+  return (
+    <nav className="flex flex-col bg-white top-0 sticky z-10 pt-1.5 shadow-lg ease-linear duration-700 opacity-100" id='header' ref={ref}>
+      <section className="flex gap-3.5 min-w-full overflow-auto scroll-remove px-2 pb-2 scrollStyle" id='nav'>
+        {navItems.map((item, index) => (
+          <button
+            key={item}
+            type="button"
+            onClick={() => { scrollToSectionNew(item) }}
+            id={`button${item}`}
+            className={`${index == buttonSelected ?
+              "bg-white px-8 py-2 text-sm font-bold text-black w-auto border-black border-2 whitespace-nowrap buttons" :
+              "bg-white px-8 py-2 text-sm font-medium text-gray-700 border-2 w-auto whitespace-nowrap buttons"} `}
+          >
+            {item}
+          </button>
+        ))}
+      </section>
+    </nav >
+  )
 })
 
 Nav.displayName = 'Nav';
